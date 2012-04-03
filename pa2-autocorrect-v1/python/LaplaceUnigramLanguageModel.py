d0cc31328736f42e8e05af1d7ac78537180b138f
korobool <korobov.alex@gmail.com>
2012-04-04 02:54:10 +0300

    Update pa2-autocorrect-v1/python/LaplaceUnigramLanguageModel.py

--- pa2-autocorrect-v1/python/LaplaceUnigramLanguageModel.py
+++ pa2-autocorrect-v1/python/LaplaceUnigramLanguageModel.py
@@ -1,20 +1,32 @@
+import math, collections
+
 class LaplaceUnigramLanguageModel:
 
-  def __init__(self, corpus):
-    """Initialize your data structures in the constructor."""
-    # TODO your code here
-    self.train(corpus)
+    def __init__(self, corpus):
+        self.unigramCounts = collections.defaultdict(lambda: 0)
+        self.total = 0
+        self.train(corpus)
 
-  def train(self, corpus):
-    """ Takes a corpus and trains your language model. 
-        Compute any counts or other corpus statistics in this function.
-    """  
-    # TODO your code here
-    pass
+    def train(self, corpus):
+        """Takes a HolbrookCorpus corpus, does whatever training is needed."""
+        for sentence in corpus.corpus:
+            for datum in sentence.data:
+                token = datum.word
+                self.unigramCounts[token] = self.unigramCounts[token] + 1
+                self.total += 1
 
-  def score(self, sentence):
-    """ Takes a list of strings as argument and returns the log-probability of the 
-        sentence using your language model. Use whatever data you computed in train() here.
-    """
-    # TODO your code here
-    return 0.0
+    def score(self, sentence):
+        """Takes a list of strings, returns a score of that sentence."""
+        score = 0.0
+        l = len(self.unigramCounts)
+
+        for token in sentence:
+            count = self.unigramCounts[token]
+            if count > 0:
+                score += math.log(count+1)
+                score -= math.log(self.total+l)
+            else:
+                score += math.log(1)
+                score -= math.log(self.total+l)
+
+        return score
